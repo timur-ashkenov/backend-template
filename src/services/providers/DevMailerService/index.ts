d8370c55f112b774ad0a640b8585f65b0dcf87ab
Тимур Ашкенов <shivada727@Air-Timur.sknt.ru>
2025-08-29 14:37:50 +0300

    fix:Deleted DevMailService

--- src/services/providers/DevMailerService/index.ts
+++
@@ -1,8 +0,0 @@
-import { IMailService } from '../../IMailService';
-
-export class DevMailService implements IMailService {
-    async sendVerificationCode(email: string, code: string): Promise<void> {
-        const masked = email.replace(/^(.).+(@.+)$/, '$1***$2');
-        console.log(`[DEV MAIL] to=${masked} code=${code}`);
-    }
-}
